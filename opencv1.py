8514d3991a34c3532e0567ce97ad25f11f031281
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2022-01-19 21:30:12 +0300

    Create opencv1.py

---
+++ opencv1.py
@@ -0,0 +1,14 @@
+#opencv1
+import cv2
+import numpy as np
+"""
+img=cv2.imread("img adresses")
+cv2.imshow("titel",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+
+img=cv2.imread("totoro.jpg")
+cv2.imshow("totoro",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
